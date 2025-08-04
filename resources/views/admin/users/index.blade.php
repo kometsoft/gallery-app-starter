98093db0f74fed6703bd9ac1f693234370107acb
syd <sayed@kometsoft.com.my>
2025-08-04 14:56:08 +0800

    feat: remove pagination

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -114,38 +114,7 @@
         </div>
 
         <!-- PAGINATION -->
-        <div class="d-flex justify-content-center mt-5">
-          <ul class="pagination">
-            <li class="page-item disabled">
-              <a class="page-link" href="#" tabindex="-1" aria-disabled="true">
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-1">
-                  <path d="M15 6l-6 6l6 6"></path>
-                </svg>
-              </a>
-            </li>
-            <li class="page-item">
-              <a class="page-link" href="#">1</a>
-            </li>
-            <li class="page-item">
-              <a class="page-link" href="#">2</a>
-            </li>
-            <li class="page-item active">
-              <a class="page-link" href="#">3</a>
-            </li>
-            <li class="page-item">
-              <a class="page-link" href="#">4</a>
-            </li>
-            <li class="page-item">
-              <a class="page-link" href="#">5</a>
-            </li>
-            <li class="page-item">
-              <a class="page-link" href="#">
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-1">
-                  <path d="M9 6l6 6l-6 6"></path>
-                </svg>
-              </a>
-            </li>
-          </ul>
+        <div class="mt-5">
         </div>
         <!-- END PAGINATION -->
 
